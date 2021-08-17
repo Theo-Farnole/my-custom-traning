71236934b59c927b4f1f93fe495bc908299ea7b6
TF <theo.farnole@gmail.com>
2021-08-17 12:00:18 +0200

    ADD creation workout modal in Workouts tab

--- src/pages/Workouts.tsx
+++ src/pages/Workouts.tsx
@@ -1,9 +1,12 @@
-import { IonButton, IonCheckbox, IonContent, IonHeader, IonIcon, IonInput, IonItem, IonItemOption, IonItemOptions, IonItemSliding, IonLabel, IonList, IonListHeader, IonPage, IonRadio, IonReorder, IonReorderGroup, IonTitle, IonToggle, IonToolbar } from '@ionic/react';
+import { IonButton, IonCheckbox, IonContent, IonHeader, IonIcon, IonInput, IonItem, IonItemOption, IonItemOptions, IonItemSliding, IonLabel, IonList, IonListHeader, IonModal, IonPage, IonRadio, IonReorder, IonReorderGroup, IonTitle, IonToggle, IonToolbar } from '@ionic/react';
 import { pizza, star } from 'ionicons/icons';
+import { useState } from 'react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Workouts.css';
 
 const Workouts: React.FC = () => {
+  const [showModal, setShowModal] = useState(false);
+
   return (
     <IonPage>
       <IonContent fullscreen>
@@ -37,7 +40,27 @@ const Workouts: React.FC = () => {
             </IonLabel>
           </IonItem>
 
-          <IonButton expand="block">Create a new workout</IonButton>
+          <IonButton expand="block" onClick={() => setShowModal(true)}>Create a new workout</IonButton>
+
+          <IonModal isOpen={showModal} cssClass='workout-name'>
+
+            <IonList>
+              <IonListHeader>
+                <h2 className="ion-text-center">Let's give a name to your new workout!</h2>
+              </IonListHeader>
+
+              <IonItem>
+                <IonLabel>Workout name</IonLabel>
+                <IonInput placeholder="Enter name" clearInput></IonInput>
+              </IonItem>
+
+              <IonItem className="ion-text-center">
+
+                <IonButton color="success" onClick={() => setShowModal(false)}>Create</IonButton>
+                <IonButton color="danger" onClick={() => setShowModal(false)}>Cancel</IonButton>
+              </IonItem>
+            </IonList>
+          </IonModal>
         </IonList>
 
       </IonContent>
