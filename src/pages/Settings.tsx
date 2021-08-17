660b84aa9f0204f5d654f1a92d60ab0c2a8b947c
TF <theo.farnole@gmail.com>
2021-08-17 11:39:22 +0200

    ADD layout in Settings tab

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,22 +1,30 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonAvatar, IonChip, IonContent, IonHeader, IonIcon, IonItem, IonItemDivider, IonLabel, IonList, IonPage, IonTitle, IonToggle, IonToolbar } from '@ionic/react';
+import { pin, heart, closeCircle } from 'ionicons/icons';
 import ExploreContainer from '../components/ExploreContainer';
 import './Settings.css';
 
 const Settings: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 3</IonTitle>
-        </IonToolbar>
-      </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 3</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
+        <h1 className="ion-text-center">
+          Settings
+        </h1>
+
+        <IonList>
+
+          { /* <IonItemDivider>Toggles in a List</IonItemDivider> */}
+
+          <IonItem>
+            <IonLabel>Dark mode</IonLabel>
+            <IonToggle checked value="dark-mode" />
+          </IonItem>
+
+          <IonItem>
+            <IonLabel>Sound alert when timer over</IonLabel>
+            <IonToggle checked value="sound" />
+          </IonItem>
+        </IonList>
       </IonContent>
     </IonPage>
   );
