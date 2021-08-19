990414dde11b10ebd987b1182dc808e7fb6cf845
TF <theo.farnole@gmail.com>
2021-08-19 16:36:40 +0200

    ADD list layout on page/EditWorkout

--- src/pages/EditWorkout.tsx
+++ src/pages/EditWorkout.tsx
@@ -1,4 +1,4 @@
-import { IonPage } from '@ionic/react';
+import { IonButton, IonContent, IonInput, IonItem, IonLabel, IonList, IonListHeader, IonPage, IonReorder, IonReorderGroup } from '@ionic/react';
 import { exception } from 'console';
 import { useState } from 'react';
 import { RouteComponentProps } from 'react-router';
@@ -7,11 +7,23 @@ import { Workout } from '../services/Workout';
 import { WorkoutsSave } from '../services/WorkoutsSave';
 import './EditWorkout.css';
 import Workouts from './Workouts';
+import { ItemReorderEventDetail } from '@ionic/core';
 
 
 interface EditWorkoutProps extends RouteComponentProps<{
     id: string;
 }> { }
+
+function doReorder(event: CustomEvent<ItemReorderEventDetail>) {
+    // The `from` and `to` properties contain the index of the item
+    // when the drag started and ended, respectively
+    console.log('Dragged from index', event.detail.from, 'to', event.detail.to);
+
+    // Finish the reorder and position the item in the DOM based on
+    // where the gesture ended. This method can also be called directly
+    // by the reorder group
+    event.detail.complete();
+}
 
 
 const EditWorkout: React.FC<EditWorkoutProps> = ({ match }) => {
@@ -45,19 +57,77 @@ const EditWorkout: React.FC<EditWorkoutProps> = ({ match }) => {
 
         return (
             <IonPage>
-                <h1 className="ion-text-center">
-                    editing workout {workout.name}
-                </h1>
-            </IonPage>
+                <IonContent>
+                    <h1 className="ion-text-center">
+                        editing workout {workout.name}
+                    </h1>
+
+                    <IonReorderGroup disabled={false} onIonItemReorder={doReorder}>
+                        <IonListHeader className="set-list-header">
+                            <IonLabel className="exercice-header"><b>Exercice</b></IonLabel>
+                            <IonLabel className="rep-header"><b>Rep count</b></IonLabel>
+                            <IonLabel className="set-header"><b>Set count</b></IonLabel>
+                        </IonListHeader>
+
+                        <IonItem className="set-input">
+                            <div className="exercice-input ion-input">
+                                <IonInput placeholder="push up"></IonInput>
+                            </div>
+
+                            <div className="rep-input ion-input">
+                                <IonInput placeholder="10"></IonInput>
+                            </div>
+
+                            <div className="set-count-input ion-input">
+                                <IonInput type="number" placeholder="3"></IonInput>
+                            </div>
+                            <IonReorder slot="start" />
+                        </IonItem>
+
+                        <IonItem className="set-input">
+                            <div className="exercice-input ion-input">
+                                <IonInput placeholder="pull up"></IonInput>
+                            </div>
+
+                            <div className="rep-input ion-input">
+                                <IonInput placeholder="3"></IonInput>
+                            </div>
+
+                            <div className="set-count-input ion-input">
+                                <IonInput type="number" placeholder="3"></IonInput>
+                            </div>
+                            <IonReorder slot="start" />
+                        </IonItem>
+
+                        <IonItem className="set-input">
+                            <div className="exercice-input ion-input">
+                                <IonInput placeholder="😀"></IonInput>
+                            </div>
+
+                            <div className="rep-input ion-input">
+                                <IonInput placeholder="Max-1"></IonInput>
+                            </div>
+
+                            <div className="set-count-input ion-input">
+                                <IonInput type="number" placeholder="3"> </IonInput>
+                            </div>
+                            <IonReorder slot="start" />
+                        </IonItem>
+
+                    </IonReorderGroup>
+                </IonContent>
+            </IonPage >
 
         );
     }
     catch (err) {
         return (
             <IonPage>
-                <h1>Error, cannot load the workout of id "{id}" because it doesn't exit or is empty</h1>
-                <p>Please, return home.</p>
-                <p>{err}</p>
+                <IonContent>
+                    <h1>Error, cannot load the workout of id "{id}" because it doesn't exit or is empty</h1>
+                    <p>Please, return home.</p>
+                    <p>{err}</p>
+                </IonContent>
             </IonPage>
         )
     }
