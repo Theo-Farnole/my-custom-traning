257b2d3dcb868332ff446d5c11e0265f70412f43
TF <theo.farnole@gmail.com>
2021-08-21 15:35:54 +0200

    ADD WIP timer + buttons to Rest page

--- src/pages/exercises/Rest.tsx
+++ src/pages/exercises/Rest.tsx
@@ -1,13 +1,20 @@
-import { IonContent, IonPage } from "@ionic/react"
+import { IonButton, IonContent, IonFooter, IonPage, IonToolbar } from "@ionic/react"
 import Timer from "../../components/Timer";
+import "./Rest.css"
 
 const Rest: React.FC = () => {
 
     return (
         <IonPage>
-            <IonContent>
+            <IonContent className="ion-text-center">
+                <h1 className="ion-text-center">Rest</h1>
                 <Timer duration={10}></Timer>
             </IonContent>
+
+            <IonFooter>
+                <IonButton expand="block" color="light">Pause</IonButton>
+                <IonButton expand="block" color="danger">Skip</IonButton>
+            </IonFooter>
         </IonPage>
     )
 };
