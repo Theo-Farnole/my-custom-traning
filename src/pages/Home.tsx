6b05c1b1eec3a876d21822c66b2c4c4131f7daaf
TF <theo.farnole@gmail.com>
2021-08-24 10:02:22 +0200

    RETAKE workout select option to show workouts

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,32 +1,61 @@
+import React from 'react';
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonTabButton, IonButton, IonIcon, IonLabel, IonItem, IonSelect, IonSelectOption, IonText, IonImg } from '@ionic/react';
 import './Home.css';
+import { WorkoutsSave } from '../services/WorkoutsSave';
+import { Workout } from '../services/Workout';
 
-const Home: React.FC = () => {
-  return (
-    <IonPage>
+class Home extends React.Component {
 
-      <IonContent fullscreen>
+  state = {
+    selectOptions: []
+  }
 
-        <h1 className="ion-text-center">
-          Custom Workouts
-        </h1>
+  constructor(props: {} | Readonly<{}>) {
+    super(props);
 
-        <IonItem>
-          <IonLabel>Select workout</IonLabel>
-          <IonSelect value="Select" placeholder="Select One">
-            <IonSelectOption value="workout-01">Custom Workout 01</IonSelectOption>
-            <IonSelectOption value="workout-02">Morning Workout 02</IonSelectOption>
-          </IonSelect>
-        </IonItem>
+    this.setSelectOptions();
+    WorkoutsSave.Instance.attachOnWorkoutsModified(() => this.setSelectOptions());
+  }
 
-        <div className="ion-text-center">
-          <IonButton color="primary" className="ion-text-center">
-            <IonLabel>Start workout</IonLabel>
-          </IonButton>
-        </div>
-      </IonContent>
-    </IonPage>
-  );
-};
+  setSelectOptions() {
+    const workouts = WorkoutsSave.Instance.workouts;
+    var selectOptions = [];
+
+    for (var i = 0; i < workouts.length; i++) {
+      selectOptions.push(<IonSelectOption key={i} value={i}>{workouts[i].name}</IonSelectOption>)
+    }
+
+    this.setState({ selectOptions: selectOptions });
+
+    console.log(selectOptions);
+  }
+
+  render() {
+    return (
+      <IonPage>
+
+        <IonContent fullscreen>
+
+          <h1 className="ion-text-center">
+            Custom Workouts
+          </h1>
+
+          <IonItem>
+            <IonLabel>Select workout</IonLabel>
+            <IonSelect value="Select" placeholder="Select One">
+              {this.state.selectOptions}
+            </IonSelect>
+          </IonItem>
+
+          <div className="ion-text-center">
+            <IonButton expand="block" color="primary" className="ion-text-center">
+              <IonLabel>Start workout</IonLabel>
+            </IonButton>
+          </div>
+        </IonContent>
+      </IonPage>
+    );
+  }
+}
 
 export default Home;
