8876c617d48c35e92472e7f7cd627bf4d4ba463b
TF <theo.farnole@gmail.com>
2021-08-24 11:47:53 +0200

    RETAKE turn Workout from a FC to a class component

--- src/pages/Workouts.tsx
+++ src/pages/Workouts.tsx
@@ -1,87 +1,96 @@
-import { IonButton, IonCheckbox, IonContent, IonHeader, IonIcon, IonInput, IonItem, IonItemOption, IonItemOptions, IonItemSliding, IonLabel, IonList, IonListHeader, IonLoading, IonModal, IonPage, IonRadio, IonReorder, IonReorderGroup, IonTitle, IonToggle, IonToolbar, useIonViewDidEnter, useIonViewDidLeave, useIonViewWillEnter, useIonViewWillLeave } from '@ionic/react';
+import { IonButton, IonCheckbox, IonContent, IonHeader, IonIcon, IonInput, IonItem, IonItemOption, IonItemOptions, IonItemSliding, IonLabel, IonList, IonListHeader, IonLoading, IonModal, IonPage, IonRadio, IonReorder, IonReorderGroup, IonTitle, IonToggle, IonToolbar, useIonAlert, useIonViewDidEnter, useIonViewDidLeave, useIonViewWillEnter, useIonViewWillLeave } from '@ionic/react';
 import { useState } from 'react';
 import { WorkoutsSave } from '../services/WorkoutsSave';
 import { Workout } from "../services/Workout";
 import './Workouts.css';
 import { useConstructor } from '../services/CustomHooks';
+import React from 'react';
 
 
+class Workouts extends React.Component {
 
+  state = {
+    showModal: false,
+    workouts: [] as Workout[]
+  }
 
-const Workouts: React.FC = () => {
-  const [showModal, setShowModal] = useState(false);
-  const [workouts, setWorkouts] = useState([] as Workout[]);
+  constructor(props: {} | Readonly<{}>) {
+    super(props);
 
-  const workouts_components: JSX.Element[] = buildWorkoutsComponent(workouts);
-
-  // constructor equivalent
-  useConstructor(() => {
     WorkoutsSave.Instance.attachOnWorkoutsModified(event => {
-      setWorkouts(WorkoutsSave.Instance.workouts);
+      this.setState(
+        { workouts: WorkoutsSave.Instance.workouts });
     });
-  });
+  }
 
+  buildWorkoutsComponent(workouts: Workout[]) {
+    const output: JSX.Element[] = [];
 
-  return (
-    <IonPage>
-      <IonContent fullscreen>
-        <h1 className="ion-text-center">
-          My workouts
-        </h1>
+    workouts.forEach(element => {
+      output.push(<IonItem key={element.name}> {/*need key property to avoid this https://sentry.io/answers/unique-key-prop/*/}
+        <IonLabel>{element.name}</IonLabel>
+        <IonLabel className="ion-text-center">{element.duration}</IonLabel>
+        <IonLabel class="ion-text-right">
+          <IonButton routerLink={"/edit-workout/" + workouts.indexOf(element)} color="secondary">edit</IonButton>
+          <IonButton color="danger" >delete</IonButton>
+        </IonLabel>
+      </IonItem>);
+    });
 
+    return output;
+  }
 
-        <IonList>
-          <IonListHeader class="list-header">
-            <IonLabel><b>Name</b></IonLabel>
-            <IonLabel className="ion-text-center"><b>Duration</b></IonLabel>
-            <IonLabel class="ion-text-right"><b>Actions</b></IonLabel>
-          </IonListHeader>
+  showModal(active: boolean) {
+    this.setState({ showModal: active });
+  }
 
-          {workouts_components}
+  render() {
+    const workouts_components: JSX.Element[] = this.buildWorkoutsComponent(this.state.workouts);
 
-          <IonButton expand="block" onClick={() => setShowModal(true)}>Create a new workout</IonButton>
+    return (
+      <IonPage>
+        <IonContent fullscreen>
+          <h1 className="ion-text-center">
+            My workouts
+          </h1>
 
-          <IonModal isOpen={showModal} cssClass='workout-name'>
+          <IonList>
+            <IonListHeader class="list-header">
+              <IonLabel><b>Name</b></IonLabel>
+              <IonLabel className="ion-text-center"><b>Duration</b></IonLabel>
+              <IonLabel class="ion-text-right"><b>Actions</b></IonLabel>
+            </IonListHeader>
 
-            <IonList>
-              <IonListHeader>
-                <h2 className="ion-text-center">Let's give a name to your new workout!</h2>
-              </IonListHeader>
+            {workouts_components}
 
-              <IonItem>
-                <IonLabel>Workout name</IonLabel>
-                <IonInput placeholder="Enter name" clearInput></IonInput>
-              </IonItem>
+            <IonButton expand="block" onClick={() => this.showModal(true)}>Create a new workout</IonButton>
 
-              <IonItem className="ion-text-center">
+            <IonModal isOpen={this.state.showModal} cssClass='workout-name'>
 
-                <IonButton color="success" onClick={() => setShowModal(false)}>Create</IonButton>
-                <IonButton color="danger" onClick={() => setShowModal(false)}>Cancel</IonButton>
-              </IonItem>
-            </IonList>
-          </IonModal>
-        </IonList>
+              <IonList>
+                <IonListHeader>
+                  <h2 className="ion-text-center">Let's give a name to your new workout!</h2>
+                </IonListHeader>
 
-      </IonContent>
-    </IonPage>
-  );
-};
+                <IonItem>
+                  <IonLabel>Workout name</IonLabel>
+                  <IonInput placeholder="Enter name" clearInput></IonInput>
+                </IonItem>
+
+                <IonItem className="ion-text-center">
+
+                  <IonButton color="success" onClick={() => this.showModal(false)}>Create</IonButton>
+                  <IonButton color="danger" onClick={() => this.showModal(false)}>Cancel</IonButton>
+                </IonItem>
+              </IonList>
+            </IonModal>
+          </IonList>
+
+        </IonContent>
+      </IonPage>
+    );
+  }
+}
 
 export default Workouts;
 
-function buildWorkoutsComponent(workouts: Workout[]) {
-  const output: JSX.Element[] = [];
-
-  workouts.forEach(element => {
-    output.push(<IonItem key={element.name}> {/*need key property to avoid this https://sentry.io/answers/unique-key-prop/*/}
-      <IonLabel>{element.name}</IonLabel>
-      <IonLabel className="ion-text-center">{element.duration}</IonLabel>
-      <IonLabel class="ion-text-right">
-        <IonButton routerLink={"/edit-workout/" + workouts.indexOf(element)} color="secondary">edit</IonButton>
-        <IonButton color="danger">delete</IonButton>
-      </IonLabel>
-    </IonItem>);
-  });
-
-  return output;
-}
