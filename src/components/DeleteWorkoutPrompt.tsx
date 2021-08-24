b8febf9953c3360292fd63322e36402222829585
TF <theo.farnole@gmail.com>
2021-08-24 15:27:31 +0200

    FIX name could make the delete prompt so tall that user could access to "delete" button

--- src/components/DeleteWorkoutPrompt.tsx
+++ src/components/DeleteWorkoutPrompt.tsx
@@ -14,8 +14,8 @@ const DeleteWorkoutPrompt: React.FC<DeleteWorkoutPromptProps> = ({ workout, isOp
         <IonAlert
             isOpen={isOpen}
             onDidDismiss={onDismiss}
-            header={'Delete ' + workout.name + '?'}
-            message={'<p>This operation cannot be reverted.</p>Are you sure to delete <strong>' + workout.name + '</strong>?'}
+            header={'Delete ' + truncateString(workout.name, 30) + '?'}
+            message={'<p>This operation cannot be reverted.</p>Are you sure to delete <strong>' + truncateString(workout.name, 30) + '</strong>?'}
             buttons={[
                 {
                     text: 'Cancel',
@@ -32,5 +32,15 @@ const DeleteWorkoutPrompt: React.FC<DeleteWorkoutPromptProps> = ({ workout, isOp
         />
     );
 };
+
+function truncateString(str: string, num: number) {
+    // If the length of str is less than or equal to num
+    // just return str--don't truncate it.
+    if (str.length <= num) {
+      return str
+    }
+    // Return str truncated with '...' concatenated to the end of str.
+    return str.slice(0, num) + '...'
+  }
 
 export default DeleteWorkoutPrompt;
