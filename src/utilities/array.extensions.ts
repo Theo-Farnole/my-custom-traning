9b721a60afecb4d420c0fab72b2e66ca51910ed0
TF <theo.farnole@gmail.com>
2021-08-26 09:36:17 +0200

    ADD UNTESTED extension Array.remove

--- src/utilities/array.extensions.ts
+++ src/utilities/array.extensions.ts
@@ -1,5 +1,6 @@
 interface Array<T> {
     move(from: number, to: number): Array<T>;
+    remove<T>(element: T): Array<T>
 }
 
 // source: https://stackoverflow.com/questions/5306680/move-an-array-element-from-one-array-position-to-another?rq=1
@@ -7,5 +8,15 @@ Array.prototype.move = function (from: number, to: number) {
     this.splice(to, 0, this.splice(from, 1)[0]);
     return this;
 };
+
+Array.prototype.remove = function <T>(element: T) {
+    const index = this.indexOf(element);
+
+    if (index > -1) {
+        this.slice(index, 1);
+    }
+
+    return this;
+}
 
 export { }
--- src/utilities/typings.d.ts
+++ src/utilities/typings.d.ts
@@ -1,3 +1,4 @@
 interface Array<T> {
     move(from: number, to: number): Array<T>;
+    remove<T>(element: T): Array<T>
 }
