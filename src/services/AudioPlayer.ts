161ed33f77ed641fe7b64b40ce51b564314096d7
TF <theo.farnole@gmail.com>
2021-09-06 15:23:16 +0200

    REMOVE unused plugins

--- src/services/AudioPlayer.ts
+++ src/services/AudioPlayer.ts
@@ -1,7 +1,3 @@
-import { NativeAudio } from '@capacitor-community/native-audio'
-
-const ASSET_ID_TIMER_ENDED = "timer_ended";
-
 export class AudioPlayer {
     static timerEnded: HTMLAudioElement = new Audio("../assets/timer_ended.mp3");
     static timerDecount: HTMLAudioElement = new Audio("../assets/timer_decount.mp3");
