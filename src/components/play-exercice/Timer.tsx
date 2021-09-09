c6b4875477308721e17be156176e4ebc8e640268
TF <theo.farnole@gmail.com>
2021-09-09 17:06:58 +0200

    FIX timer was too big: make it responsive w/out overlapping button

--- src/components/play-exercice/Timer.tsx
+++ src/components/play-exercice/Timer.tsx
@@ -113,7 +113,7 @@ class Timer extends React.Component<TimerProps> {
 
     render() {
         return (
-            <div className="timer">
+            <div className="timer squarred">
                 <svg className="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
                     <circle className="background" r="45" cx="50" cy="50"></circle>
 
