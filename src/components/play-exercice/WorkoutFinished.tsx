c3e118916ef4c23b0926ac833c651db740767f3a
TF <theo.farnole@gmail.com>
2021-09-10 10:43:07 +0200

    RETAKE WIP make success animation responsive

--- src/components/play-exercice/WorkoutFinished.tsx
+++ src/components/play-exercice/WorkoutFinished.tsx
@@ -14,18 +14,18 @@ class Rest extends React.Component {
                 <IonContent>
                     {/* source https://blog.prototypr.io/delighting-students-with-a-twitter-like-css-only-animation-diy-kit-included-a5fcae0d6017#.thzx6cmbd */}
 
-                    <div className="success-animation">
-                        <div className="success-checkmark">
-                            <div className="check-icon">
-                                <span className="icon-line line-tip"></span>
-                                <span className="icon-line line-long"></span>
-                                <div className="icon-circle"></div>
-                                <div className="icon-fix"></div>
+                    <div className="informations">
+                        <div className="success-animation">
+                            <div className="success-checkmark">
+                                <div className="check-icon">
+                                    <span className="icon-line line-tip"></span>
+                                    <span className="icon-line line-long"></span>
+                                    <div className="icon-circle"></div>
+                                    <div className="icon-fix"></div>
+                                </div>
                             </div>
                         </div>
-                    </div>
 
-                    <div className="informations">
                         <h1 className="ion-text-center">Great job! 🎉</h1>
                         <p className="ion-text-center">
                             You have completed your workout.<br />
