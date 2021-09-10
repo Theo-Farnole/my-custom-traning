24e4459c1beec4b4b9c147a6c9e2bc4a2790afe4
TF <theo.farnole@gmail.com>
2021-09-10 11:18:16 +0200

    RETAKE layout of WorkoutFinished to be more responsive

--- src/components/play-exercice/WorkoutFinished.tsx
+++ src/components/play-exercice/WorkoutFinished.tsx
@@ -14,8 +14,8 @@ class Rest extends React.Component {
                 <IonContent>
                     {/* source https://blog.prototypr.io/delighting-students-with-a-twitter-like-css-only-animation-diy-kit-included-a5fcae0d6017#.thzx6cmbd */}
 
-                    <div className="informations">
-                        <div className="success-animation">
+                    <div className="informations flex-container">
+                        <div className="success-animation flex-item">
                             <div className="success-checkmark">
                                 <div className="check-icon">
                                     <span className="icon-line line-tip"></span>
@@ -26,12 +26,14 @@ class Rest extends React.Component {
                             </div>
                         </div>
 
-                        <h1 className="ion-text-center">Great job! 🎉</h1>
-                        <p className="ion-text-center">
-                            You have completed your workout.<br />
-                            Keep it up!
-                        </p>
-                        <IonButton routerLink="/home/" onClick={() => AdsPlayer.interstitial()} className="action continue" expand="block">Continue</IonButton>
+                        <div className="flex-item">
+                            <h1 className="ion-text-center">Great job! 🎉</h1>
+                            <p className="ion-text-center">
+                                You have completed your workout.<br />
+                                Keep it up!
+                            </p>
+                            <IonButton routerLink="/home/" onClick={() => AdsPlayer.interstitial()} className="action continue" expand="block">Continue</IonButton>
+                        </div>
                     </div>
 
 
