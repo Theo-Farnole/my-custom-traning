2460b7f1fd9da65ad1c03f8bc8949d0e7b32cc46
TF <theo.farnole@gmail.com>
2021-09-15 13:51:13 +0200

    RETAKE editing workout layout (tris)

--- src/components/edit-workout/SetItem.tsx
+++ src/components/edit-workout/SetItem.tsx
@@ -59,23 +59,23 @@ class SetItem extends React.Component<SetItemProps> {
                 <IonItem lines="none" key={set.uid} className="setContainer">
                     <IonGrid>
                         <IonRow>
-                            <IonCol>
-                                <IonInput disabled={!this.props.isEditing} className="input text" placeholder="exercise" onIonChange={e => { set.exercise = e.detail.value as string; WorkoutsSave.Instance.saveCurrentWorkouts(); }} value={set.exercise}></IonInput>
-                            </IonCol>
+                        <IonCol>
+                        <IonInput disabled={!this.props.isEditing} className="input text" placeholder="exercise" onIonChange={e => { set.exercise = e.detail.value as string; WorkoutsSave.Instance.saveCurrentWorkouts(); }} value={set.exercise}></IonInput>
+                        </IonCol>
 
-                            <IonCol>
-                                <IonRow>
-                                    <IonLabel className="inputLabel">Rep</IonLabel>
-                                    <IonInput disabled={!this.props.isEditing} className="input number rep" placeholder="1" onIonChange={e => { set.repetitionsPerSet = e.detail.value as string; WorkoutsSave.Instance.saveCurrentWorkouts(); }} value={set.repetitionsPerSet}></IonInput>
-                                </IonRow>
-                            </IonCol>
+                        <IonCol>
+                        <IonRow>
+                        <IonLabel className="inputLabel">Rep</IonLabel>
+                        <IonInput disabled={!this.props.isEditing} className="input number rep" placeholder="1" onIonChange={e => { set.repetitionsPerSet = e.detail.value as string; WorkoutsSave.Instance.saveCurrentWorkouts(); }} value={set.repetitionsPerSet}></IonInput>
+                        </IonRow>
+                        </IonCol>
 
-                            <IonCol>
-                                <IonRow>
-                                    <IonLabel>Set</IonLabel>
-                                    <IonInput disabled={!this.props.isEditing} className="input number" placeholder="1" type="number" onIonChange={e => { set.setCount = parseInt(e.detail.value as string); WorkoutsSave.Instance.saveCurrentWorkouts(); }} value={set.setCount}></IonInput>
-                                </IonRow>
-                            </IonCol>
+                        <IonCol>
+                        <IonRow>
+                        <IonLabel>Set</IonLabel>
+                        <IonInput disabled={!this.props.isEditing} className="input number" placeholder="1" type ="number" onIonChange={e => { set.setCount = parseInt(e.detail.value as string); WorkoutsSave.Instance.saveCurrentWorkouts(); }} value={set.setCount}></IonInput>
+                        </IonRow>
+                        </IonCol>
                         </IonRow>
                     </IonGrid>
 
