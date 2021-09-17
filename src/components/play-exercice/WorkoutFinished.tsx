f404eb96738afd7619546c6ea1edafea5cbe5b3e
TF <theo.farnole@gmail.com>
2021-09-17 11:47:55 +0200

    FIX missing checkmark in mobile app (css min() wasnt supported)

--- src/components/play-exercice/WorkoutFinished.tsx
+++ src/components/play-exercice/WorkoutFinished.tsx
@@ -11,41 +11,37 @@ class Rest extends React.Component {
         AdsPlayer.prepareInterstitial_WorkoutEnded();
 
         return (
-            <IonPage>
-                <IonContent>
-                    {/* source https://blog.prototypr.io/delighting-students-with-a-twitter-like-css-only-animation-diy-kit-included-a5fcae0d6017#.thzx6cmbd */}
+            <>
+                {/* source https://blog.prototypr.io/delighting-students-with-a-twitter-like-css-only-animation-diy-kit-included-a5fcae0d6017#.thzx6cmbd */}
 
-                    <div className="informations flex-container">
-                        <div className="success-animation flex-item">
-                            <div className="success-checkmark">
-                                <div className="check-icon">
-                                    <span className="icon-line line-tip"></span>
-                                    <span className="icon-line line-long"></span>
-                                    <div className="icon-circle"></div>
-                                    <div className="icon-fix"></div>
-                                </div>
+                <div className="informations flex-container">
+                    <div className="success-animation flex-item">
+                        <div className="success-checkmark">
+                            <div className="check-icon">
+                                <span className="icon-line line-tip"></span>
+                                <span className="icon-line line-long"></span>
+                                <div className="icon-circle"></div>
+                                <div className="icon-fix"></div>
                             </div>
-                        </div>
-
-                        <div className="flex-item">
-                            <h1 className="ion-text-center">Great job! 🎉</h1>
-                            <p className="ion-text-center">
-                                You have completed your workout.<br />
-                                Keep it up!
-                            </p>
-                            <IonButton routerLink="/home/" onClick={() => {
-                                FirebaseAnalytics.logEvent({
-                                    name: "workout_completed",
-                                    params: {}
-                                });
-                                AdsPlayer.interstitial()
-                            }} className="action continue" expand="block">Continue</IonButton>
                         </div>
                     </div>
 
-
-                </IonContent>
-            </IonPage>
+                    <div className="flex-item">
+                        <h1 className="ion-text-center">Great job! 🎉</h1>
+                        <p className="ion-text-center">
+                            You have completed your workout.<br />
+                            Keep it up!
+                        </p>
+                        <IonButton routerLink="/home/" onClick={() => {
+                            FirebaseAnalytics.logEvent({
+                                name: "workout_completed",
+                                params: {}
+                            });
+                            AdsPlayer.interstitial()
+                        }} className="action continue" expand="block">Continue</IonButton>
+                    </div>
+                </div>
+            </>
         )
     }
 }
